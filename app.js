b1aa5c828ca0f908804a7cff9e8e9fb4818f1b56
JoseSimonDiaz <josesimondiaz13@gmail.com>
2024-10-06 01:36:16 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const input = document.querySelector(".input-text");
+const addForm = document.querySelector(".add-form");
+const taskList = document.querySelector(".tasks-list");
+const deleteBtn = document.querySelector(".deleteAll-btn");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || []; 
+
+const saveOnLocalStorage = (taskList) => {
+    localStorage.setItem("tasks", JSON.stringify(taskList)); 
+}
+
+const createTask = (task) => {
+    return `<li>${task.name} <img class="delete-btn" src="./img/delete.svg" alt="boton de borrar" data-name=${task.name}></li>`;
+}
+
+const renderTasksList = (todoList) => {
+    taskList.innerHTML = todoList.map((task) => createTask(task)).join("");
+}
+
+const hideDeletAll = (tasksList) => {
+    if (!tasksList.length) {
+        deleteBtn.classList.add("hidden");
+        return;
+    }
+    deleteBtn.classList.remove("hidden");
+}
+
+const addTask = (e) => {
+    e.preventDefault();
+    const taskname = input.value.trim();
+    if (!taskname.length) {
+        alert("Ingrese una tarea");
+        return;
+    } else if (tasks.some((task) => task.name.toLowerCase() === taskname.toLowerCase())) {
+        alert("Ya existe una tarea con ese nombre");
+        return;
+    }
+
+    tasks = [...tasks, { name: taskname }];
+    input.value = "";
+    updateViews(tasks);
+}
+
+const removeTask = (e) => {
+    if (!e.target.classList.contains("delete-btn")) {
+        return;
+    }
+    const filterName = e.target.dataset.name;
+
+    tasks = tasks.filter((task) => task.name !== filterName);
+    updateViews(tasks);
+}
+
+const removeAll = () => {
+    tasks = [];
+    updateViews(tasks);
+}
+
+const updateViews = (tasks) => {
+    renderTasksList(tasks);
+    saveOnLocalStorage(tasks);
+    hideDeletAll(tasks);
+}
+
+const init = () => {
+    renderTasksList(tasks);
+    addForm.addEventListener("submit", addTask);
+    taskList.addEventListener("click", removeTask);
+    deleteBtn.addEventListener("click", removeAll);
+}
+
+init();
